f704326a9ff9ab96643c8e7971548cd75faa5449
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2024-08-13 18:35:39 +0300

    basic project structure

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+# game loop
+class Game
+  def initialize(name1, name2)
+    @player1_name = name1
+    @player2_name = name2
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+  def self.start
+    puts "hello"
+  end
+end
